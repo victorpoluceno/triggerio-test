c4d2b24a133436adf6e1bbdb581e1ad8d3509a21
Victor Godoy Poluceno <victorpoluceno@gmail.com>
2012-07-01 21:43:18 -0300

    Import project

---
+++ development/ios/device-ios.app/assets/src/js/main.js
@@ -0,0 +1,15 @@
+forge.logging.info("Add JavaScript to js/main.js!");
+
+Pouch('idb://test', function(err, db) {
+  // Use db to call further functions
+  forge.logging.info("Horay");
+})
+
+$(document).ready(function(){
+    new GMaps({
+      div: '#map',
+      lat: -12.043333,
+      lng: -77.028333,
+      height: '200px'
+    });
+});
